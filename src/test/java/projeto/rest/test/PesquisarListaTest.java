294f529af2eb104e288736ad10f8be66aa594ab1
vtvraax <vitorrodriguessp112@gmail.com>
2024-02-28 21:55:09 -0300

    Atualizando pom.xml

--- src/test/java/projeto/rest/test/PesquisarListaTest.java
+++ src/test/java/projeto/rest/test/PesquisarListaTest.java
@@ -1,8 +1,10 @@
 package projeto.rest.test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
